3ffec9a5c3c9d0dd127b42a4105104127b907525
Gaye Lowenstein <glowen@hotmail.com>
2016-10-28 17:15:12 -0700

    adds user board story models

---
+++ db/migrate/20161028232950_create_stories.rb
@@ -0,0 +1,13 @@
+class CreateStories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.date :date
+      t.string :description
+      t.references :user, foreign_key: true
+      t.references :board, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
