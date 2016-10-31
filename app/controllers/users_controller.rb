477542ff27d97af755456933efa9c5a06ef543c3
Gaye Lowenstein <glowen@hotmail.com>
2016-10-31 10:49:20 -0700

    adds password authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
     end
   end
 
+private
   def user_params
-    params_require(:user).permit(:name, :email, :mobile, :address, :dob, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :mobile, :address, :dob, :password, :password_confirmation)
   end
 end
