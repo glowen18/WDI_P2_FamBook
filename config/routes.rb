4daed8f816c4f3860d6870176a7990285b8e671e
Gaye Lowenstein <glowen@hotmail.com>
2016-10-31 11:39:58 -0700

    adds login, signup and logout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
+  get '/login' => 'sessions#new', as: :new_session
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :users
   resources :boards do
     resources :stories
