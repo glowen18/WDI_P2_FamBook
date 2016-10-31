ad6f8a50842f7ea31aa4a0c0268ee67f97c253ed
Gaye Lowenstein <glowen@hotmail.com>
2016-10-31 12:58:14 -0700

    adds edit and update to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,9 @@ class SessionsController < ApplicationController
     end
   end
 
+
+
+
   def destroy
     session.delete(:user_id)
     redirect_to root_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params.require(:user).permit(:name, :email, :mobile, :address, :dob, :password, :password_confirmation))
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
 private
   def user_params
     params.require(:user).permit(:name, :email, :mobile, :address, :dob, :password, :password_confirmation)
