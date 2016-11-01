852952d6be5dcf57259c45d49f2f620b3f9cdd7d
Gaye Lowenstein <glowen@hotmail.com>
2016-11-01 16:18:29 -0700

    sets up static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  root 'welcome#index'
 
   get '/login' => 'sessions#new', as: :new_session
   post '/login' => 'sessions#create'
