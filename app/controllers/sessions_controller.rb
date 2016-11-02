1e9f39d44b4db23ebc70e049746ebb422bc22fe7
Gaye Lowenstein <glowen@hotmail.com>
2016-11-02 16:41:18 -0700

    makes it so error message shows with incorrect login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:login][:email])
+    user = User.find_by(email: params[:login][:email].downcase)
     if user && user.authenticate(params[:login][:password])
-      session[:user_id] = user.id.to_s
+      log_in user
+      # session[:user_id] = user.id.to_s
       redirect_to users_path
     else
+      flash.now[:danger] = 'Invalid email/password combo'
      render :new
    end
   end
