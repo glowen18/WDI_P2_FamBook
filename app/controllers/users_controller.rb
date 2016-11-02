726b6d2a24982fc900655273c008b47db0c6d94e
Gaye Lowenstein <glowen@hotmail.com>
2016-11-02 15:59:18 -0700

    updates user login page to direct to welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
 # GET /users/1
   def show
-    @user = User.find(params[:id])
+    @user = set_user
     @board = Board.new
     @story = Story.new
   end
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
 # PATCH /users/1
   def update
-    if @user.update_attributes(params.require(:user).permit(:name, :email, :mobile, :address, :dob, :password, :password_confirmation))
+    if @user.update_attributes(user_params)
       redirect_to users_path
     else
       render :edit
@@ -56,16 +56,16 @@ class UsersController < ApplicationController
     end
   end
 
-private
-  def set_user
-    @user = User.find(params[:id])
-  end
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :mobile, :address, :dob,  :password, :password_confirmation)
+    end
 
-  def redirect_to_default
-    redirect_to root_path
-  end
+    def redirect_to_default
+      redirect_to root_path
+    end
 end
