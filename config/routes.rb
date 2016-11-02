73f92da48a179fbd4c15860e430dbf41f8363e35
Gaye Lowenstein <glowen@hotmail.com>
2016-11-01 22:32:45 -0700

    adds contact page and refactors layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 
   get '/login' => 'sessions#new', as: :new_session
   post '/login' => 'sessions#create'
