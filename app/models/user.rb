46e7676f4fffa807fdce6f40396967fb0d0bf8bf
Gaye Lowenstein <glowen@hotmail.com>
2016-11-02 13:58:48 -0700

    updates error messages when no name or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_and_belongs_to_many :boards
   has_many :stories
+  validates(:name, presence: true)
+  validates(:email, presence: true)
 end
